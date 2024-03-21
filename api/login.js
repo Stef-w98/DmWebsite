8b187a6f20b2e983227cbf40b1a4873d461fc396
Stef-w98 <stef.wouters18@gmail.com>
2024-03-21 20:34:40 +0100

    changes login working

--- api/login.js
+++ api/login.js
@@ -2,12 +2,12 @@
 
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://nhgspooltizwismypzan.supabase.co;' // Use environment variables
+const supabaseUrl = 'https://nhgspooltizwismypzan.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oZ3Nwb29sdGl6d2lzbXlwemFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA2MTI2MDQsImV4cCI6MjAyNjE4ODYwNH0.7uPvzyXlBh6EUShss-I2KkuAAPdyeMauKXdKwGl6YnA' // Use environment variables
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 module.exports = async (req, res) => {
-    // Set CORS headers for every response
+    // Ensure the CORS headers are set for every response
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -32,7 +32,7 @@ module.exports = async (req, res) => {
         }
 
         console.log('User logged in successfully:', user);
-        return res.status(200).json({ user });
+        return res.status(200).json({ user: user });
     } else {
         // Respond with method not allowed for non-POST requests
         res.setHeader('Allow', ['POST']);
